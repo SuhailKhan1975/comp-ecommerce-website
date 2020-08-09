6a6617a6248dddc015bba96215fa906b764a52f3
Mehwish-nedian <49073146+Mehwish-nedian@users.noreply.github.com>
2020-08-09 12:19:47 +0500

    Updating changes

--- m-dev-store/customer/confirm.php
+++ m-dev-store/customer/confirm.php
@@ -1,61 +1,135 @@
+<?php 
+
+session_start();
+
+if(!isset($_SESSION['customer_email'])){
+    
+    echo "<script>window.open('../checkout.php','_self')</script>";
+    
+}else{
+
+include("includes/db.php");
+include("functions/functions.php");
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>M-Dev Store</title>
     <link rel="stylesheet" href="styles/bootstrap-337.min.css">
     <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
     <link rel="stylesheet" href="styles/style.css">
-    <title>Document</title>
-
-
 </head>
 <body>
+   
+   <div id="top"><!-- Top Begin -->
+       
+       <div class="container"><!-- container Begin -->
+           
+           <div class="col-md-6 offer"><!-- col-md-6 offer Begin -->
+               
+               <a href="#" class="btn btn-success btn-sm">
+                   
+                   <?php 
+                   
+                   if(!isset($_SESSION['customer_email'])){
+                       
+                       echo "Welcome: Guest";
+                       
+                   }else{
+                       
+                       echo "Welcome: " . $_SESSION['customer_email'] . "";
+                       
+                   }
+                   
+                   ?>
+               
+               </a>
+               <a href="checkout.php"> <?php items(); ?> Items In Your Cart | Total Price: <?php total_price(); ?> </a>
+               
+           </div><!-- col-md-6 offer Finish -->
+           
+           <div class="col-md-6"><!-- col-md-6 Begin -->
+               
+               <ul class="menu"><!-- cmenu Begin -->
+                   
+                   <li>
+                       <a href="../customer_register.php">Register</a>
+                   </li>
+                   <li>
+                       <a href="my_account.php">My Account</a>
+                   </li>
+                   <li>
+                       <a href="../cart.php">Go To Cart</a>
+                   </li>
+                   <li>
+                       <a href="../checkout.php">
+                       
+                        <?php 
+                           
+                           if(!isset($_SESSION['customer_email'])){
+                       
+                                echo "<a href='checkout.php'> Login </a>";
 
-    <div id="top">
-        <div class="container">
-            <div class="col-md-6 offer">
-                <a href="#" class="btn btn-sm btn-success">Wellcome</a>
-                <a href="checkout.php">4 items in your cart | Total price is 300$ </a>
-            </div><!--col-md-6 offer end-->
-            <div class="col-md-6">
+                               }else{
 
-                <ul class="menu">
-                    <li><a href="../customer_register.php"> Register | </a></li>
-                    <li><a href="my_account.php"> My Account | </a></li>
-                    <li><a href="../cart.php"> Go To Cart | </a></li>
-                    <li><a href="../checkout.php"> Login Page</a></li>
+                                echo " <a href='logout.php'> Log Out </a> ";
 
-                </ul><!--menu end-->
-
-            </div><!--col-md-6 end-->
-        </div><!-- container-end-->
-    </div><!--Top End-->
-
-    <div id = "navbar" class="navbar navbar-default">
-        <div class="container">
-            <div class = "navbar-header">
-                <a href="index.php" class="navbar-brand home">
-                <img src="images/ecom-store-logo.png" alt="Site logo" class = "hidden-xs">
-                <img src="images/ecom-store-logo-mobile.png" alt="Site logo" class="visible-xs">
-                </a><!--navbar-brand home end-->
-                <button class="navbar-toggle" data-toggle = "collapse" data-target="#navigation">
-                    <span class = "sr-only">Toggle Navigation</span>
-                    <i class="fa fa-align-justify"></i>
-                </button>
-
-                <button class="navbar-toggle" data-toggle = "collapse" data-target="#search">
-                    <span class = "sr-only">Toggle Search</span>
-                    <i class="fa fa-search"></i>
-                </button>
-
-            </div><!--navbar-header end-->
-
-            <div class="navbar-collapse collapse" id="navigation">
-                <div class="padding-nav">
-                    <ul class="nav navbar-nav left">
-                    <li>
+                               }
+                           
+                         ?>
+                       
+                       </a>
+                   </li>
+                   
+               </ul><!-- menu Finish -->
+               
+           </div><!-- col-md-6 Finish -->
+           
+       </div><!-- container Finish -->
+       
+   </div><!-- Top Finish -->
+   
+   <div id="navbar" class="navbar navbar-default"><!-- navbar navbar-default Begin -->
+       
+       <div class="container"><!-- container Begin -->
+           
+           <div class="navbar-header"><!-- navbar-header Begin -->
+               
+               <a href="../index.php" class="navbar-brand home"><!-- navbar-brand home Begin -->
+                   
+                   <img src="images/ecom-store-logo.png" alt="M-dev-Store Logo" class="hidden-xs">
+                   <img src="images/ecom-store-logo-mobile.png" alt="M-dev-Store Logo Mobile" class="visible-xs">
+                   
+               </a><!-- navbar-brand home Finish -->
+               
+               <button class="navbar-toggle" data-toggle="collapse" data-target="#navigation">
+                   
+                   <span class="sr-only">Toggle Navigation</span>
+                   
+                   <i class="fa fa-align-justify"></i>
+                   
+               </button>
+               
+               <button class="navbar-toggle" data-toggle="collapse" data-target="#search">
+                   
+                   <span class="sr-only">Toggle Search</span>
+                   
+                   <i class="fa fa-search"></i>
+                   
+               </button>
+               
+           </div><!-- navbar-header Finish -->
+           
+           <div class="navbar-collapse collapse" id="navigation"><!-- navbar-collapse collapse Begin -->
+               
+               <div class="padding-nav"><!-- padding-nav Begin -->
+                   
+                   <ul class="nav navbar-nav left"><!-- nav navbar-nav left Begin -->
+                       
+                       <li>
                            <a href="../index.php">Home</a>
                        </li>
                        <li>
@@ -70,135 +144,179 @@
                        <li>
                            <a href="../contact.php">Contact Us</a>
                        </li>
-                    </ul>
-                </div><!--padding-nav end-->
-                <a href="cart.php" class="btn navbar-btn btn-primary right">
-                    <i class="fa fa-shopping-cart"></i>
-                    <span>4 Items In Your Cart </span>
-                </a>
-                <div class="navbar-collapse collapse right">
-                    <button class="btn btn-primary navbar-btn" type = "button" data-toggle = "collapse" data-target = "#search">
-                        <span class="sr-only">Toggle Search</span>
-                        <i class="fa fa-search"></i>
-                    </button><!--btn btn-primary navbar-btn end-->
-                </div><!--navbar-collapse collapse right end-->
-
-                <div class="collapse clearfix" id="search">
-                    <form action="result.php" method = "get" class="navbar-form">
-                        <div class="input-group">
-                            <input type="text" class="form-control" name = "user_query" placeholder = "search" required>
-                            <span class="input-group-btn">
-                            <button type = "submit" name = "search" value = "search" class="btn btn-primary">
-                                <i class="fa fa-search"></i>
-                            </button><!--btn btn-primary end-->
-                            </span><!--input-group-btn end-->
-                        </div><!--input-group-->
-                    </form><!--navbar-form end-->
-                </div><!--collapse clearfix end-->
-
-            </div><!--navbar-collapse collapse end--->
-        </div><!--container end-->
-    </div><!--navbar end-->
-
-    <div id="content">  <!--content starts-->
-        <div class="container"><!--container end-->
-            <div class="col-md-12"> <!--col-md-12 start-->
-
-                <ul class="breadcrumb"> <!--breadcrumb start-->
-                    <li>
-                        <a href="index.php">Home</a>
-                    </li>
-                    <li>My Account</li>
-                </ul> <!--breadcrumb ends-->
-
-            </div><!--col-md-12 end-->
-
-        <div class="col-md-3"> <!--col-md-3 starts-->
-        <?php
-        include("includes/sidebar.php");
-        ?>
-        </div> <!--col-md-3 end-->
-
-        <div class="col-md-9">  <!--col-md-9 start-->
-
-          <div class="box"> <!--box start-->
-
-            <h1 align="center">Please confirm your payment</h1>
-            <form action="confirm.php" method="post" enctype="multipart/form-data"> <!--box start-->
-
-              <div class="form-group"> <!-- form-group start-->
-
-                <label> Invoice No:</label>
-                <input type="text" class="form-control" name="invoice_no:" required>
-
-              </div> <!-- form-group end-->
-              <div class="form-group"> <!-- form-group start-->
-
-                <label> Amount Sent:</label>
-                <input type="text" class="form-control" name="amount_sent" required>
-
-              </div> <!-- form-group end-->
-              <div class="form-group"> <!-- form-group start-->
-
-                <label> Select Payment Mode: </label>
-                <select class="form-control" name="payment_mode"> <!-- form-control start-->
-
-                  <option> Select Payment Mode </option>
-                  <option> Bank Code </option>
-                  <option> UBL / Omni Paisa </option>
-                  <option> Easy Paisa </option>
-                  <option> Western Union </option>
-
-                </select> <!-- form-control end-->
-
-              </div> <!-- form-group end-->
-              <div class="form-group"> <!-- form-group start-->
-
-                <label> Transaction / Reference ID: </label>
-                <input type="text" class="form-control" name="Reference_no" required>
-
-              </div> <!-- form-group end-->
-              <div class="form-group"> <!-- form-group start-->
-
-                <label> Omni Paisa / East Paisa:</label>
-                <input type="text" class="form-control" name="code" required>
-
-              </div> <!-- form-group end-->
-
-              <div class="form-group"> <!-- form-group start-->
-
-                <label> Payment Date:</label>
-                <input type="text" class="form-control" name="date" required>
-
-              </div> <!-- form-group end-->
-
-              <div class="text-center">  <!--text-center start-->
-
-                <button class="btn btn-primary btn-lg"> <!--btn btn-primary btn-lg start-->
-                  <i class="fa fa-user-md"></i> Confirm Payment
-                </button> <!--btn btn-primary btn-lg end-->
-
-              </div>  <!--text-center end-->
-
-
-
-            </form>  <!--box end-->
-
-
-
-          </div> <!--box end-->
-
-
-
-        </div> <!--col-md-9 start-->
-      </div> <!--container end-->
-  </div> <!--content end-->
-
-  <?php
-  include("includes/footer.php");
-  ?>
-  <script src = "js/jquery-331.min.js"></script>
-  <script src = "js/bootstrap-337.min.js"></script>
-
-  </body>
-  </html>
+                       
+                   </ul><!-- nav navbar-nav left Finish -->
+                   
+               </div><!-- padding-nav Finish -->
+               
+               <a href="cart.php" class="btn navbar-btn btn-primary right"><!-- btn navbar-btn btn-primary Begin -->
+                   
+                   <i class="fa fa-shopping-cart"></i>
+                   
+                   <span><?php items(); ?> Items In Your Cart</span>
+                   
+               </a><!-- btn navbar-btn btn-primary Finish -->
+               
+               <div class="navbar-collapse collapse right"><!-- navbar-collapse collapse right Begin -->
+                   
+                   <button class="btn btn-primary navbar-btn" type="button" data-toggle="collapse" data-target="#search"><!-- btn btn-primary navbar-btn Begin -->
+                       
+                       <span class="sr-only">Toggle Search</span>
+                       
+                       <i class="fa fa-search"></i>
+                       
+                   </button><!-- btn btn-primary navbar-btn Finish -->
+                   
+               </div><!-- navbar-collapse collapse right Finish -->
+               
+               <div class="collapse clearfix" id="search"><!-- collapse clearfix Begin -->
+                   
+                   <form method="get" action="results.php" class="navbar-form"><!-- navbar-form Begin -->
+                       
+                       <div class="input-group"><!-- input-group Begin -->
+                           
+                           <input type="text" class="form-control" placeholder="Search" name="user_query" required>
+                           
+                           <span class="input-group-btn"><!-- input-group-btn Begin -->
+                           
+                           <button type="submit" name="search" value="Search" class="btn btn-primary"><!-- btn btn-primary Begin -->
+                               
+                               <i class="fa fa-search"></i>
+                               
+                           </button><!-- btn btn-primary Finish -->
+                           
+                           </span><!-- input-group-btn Finish -->
+                           
+                       </div><!-- input-group Finish -->
+                       
+                   </form><!-- navbar-form Finish -->
+                   
+               </div><!-- collapse clearfix Finish -->
+               
+           </div><!-- navbar-collapse collapse Finish -->
+           
+       </div><!-- container Finish -->
+       
+   </div><!-- navbar navbar-default Finish -->
+   
+   <div id="content"><!-- #content Begin -->
+       <div class="container"><!-- container Begin -->
+           <div class="col-md-12"><!-- col-md-12 Begin -->
+               
+               <ul class="breadcrumb"><!-- breadcrumb Begin -->
+                   <li>
+                       <a href="index.php">Home</a>
+                   </li>
+                   <li>
+                       My Account
+                   </li>
+               </ul><!-- breadcrumb Finish -->
+               
+           </div><!-- col-md-12 Finish -->
+           
+           <div class="col-md-3"><!-- col-md-3 Begin -->
+   
+   <?php 
+    
+    include("includes/sidebar.php");
+    
+    ?>
+               
+           </div><!-- col-md-3 Finish -->
+           
+           <div class="col-md-9"><!-- col-md-9 Begin -->
+               
+               <div class="box"><!-- box Begin -->
+                   
+                   <h1 align="center"> Please confirm your payment</h1>
+                   
+                   <form action="confirm.php" method="post" enctype="multipart/form-data"><!-- form Begin -->
+                       
+                       <div class="form-group"><!-- form-group Begin -->
+                           
+                         <label> Invoice No: </label>
+                          
+                          <input type="text" class="form-control" name="invoice_no" required>
+                           
+                       </div><!-- form-group Finish -->
+                       
+                       <div class="form-group"><!-- form-group Begin -->
+                           
+                         <label> Amount Sent: </label>
+                          
+                          <input type="text" class="form-control" name="amount_sent" required>
+                           
+                       </div><!-- form-group Finish -->
+                       
+                       <div class="form-group"><!-- form-group Begin -->
+                           
+                         <label> Select Payment Mode: </label>
+                          
+                          <select name="payment_mode" class="form-control"><!-- form-control Begin -->
+                              
+                              <option> Select Payment Mode </option>
+                              <option> Back Code </option>
+                              <option> UBL / Omni Paisa </option>
+                              <option> Easy Paisa </option>
+                              <option> Western Union </option>
+                              
+                          </select><!-- form-control Finish -->
+                           
+                       </div><!-- form-group Finish -->
+                       
+                       <div class="form-group"><!-- form-group Begin -->
+                           
+                         <label> Transaction / Reference ID: </label>
+                          
+                          <input type="text" class="form-control" name="ref_no" required>
+                           
+                       </div><!-- form-group Finish -->
+                       
+                       <div class="form-group"><!-- form-group Begin -->
+                           
+                         <label> Omni Paisa / Easy Paisa: </label>
+                          
+                          <input type="text" class="form-control" name="code" required>
+                           
+                       </div><!-- form-group Finish -->
+                       
+                       <div class="form-group"><!-- form-group Begin -->
+                           
+                         <label> Payment Date: </label>
+                          
+                          <input type="text" class="form-control" name="date" required>
+                           
+                       </div><!-- form-group Finish -->
+                       
+                       <div class="text-center"><!-- text-center Begin -->
+                           
+                           <button class="btn btn-primary btn-lg"><!-- tn btn-primary btn-lg Begin -->
+                               
+                               <i class="fa fa-user-md"></i> Confirm Payment
+                               
+                           </button><!-- tn btn-primary btn-lg Finish -->
+                           
+                       </div><!-- text-center Finish -->
+                       
+                   </form><!-- form Finish -->
+                   
+               </div><!-- box Finish -->
+               
+           </div><!-- col-md-9 Finish -->
+           
+       </div><!-- container Finish -->
+   </div><!-- #content Finish -->
+   
+   <?php 
+    
+    include("includes/footer.php");
+    
+    ?>
+    
+    <script src="js/jquery-331.min.js"></script>
+    <script src="js/bootstrap-337.min.js"></script>
+    
+    
+</body>
+</html>
+<?php } ?>
