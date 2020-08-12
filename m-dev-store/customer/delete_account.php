2fe2cda858f9b6517f2673406a1c3517cd27c61c
MemonaKhan <suhail1975pk@yahoo.co.uk>
2020-08-12 13:43:55 +0500

    completed 32

--- m-dev-store/customer/delete_account.php
+++ m-dev-store/customer/delete_account.php
@@ -14,3 +14,35 @@
 
 
 </center><!--center end-->
+
+
+
+<?php 
+
+$c_email = $_SESSION['customer_email'];
+
+if(isset($_POST['Yes'])){
+    
+    $delete_customer = "delete from customers where customer_email='$c_email'";
+    
+    $run_delete_customer = mysqli_query($con,$delete_customer);
+    
+    if($run_delete_customer){
+        
+        session_destroy();
+        
+        echo "<script>alert('Successfully delete your account, feel sorry about this. Good Bye')</script>";
+        
+        echo "<script>window.open('../index.php','_self')</script>";
+        
+    }
+    
+}
+
+if(isset($_POST['No'])){
+    
+    echo "<script>window.open('my_account.php?my_orders','_self')</script>";
+    
+}
+
+?>
